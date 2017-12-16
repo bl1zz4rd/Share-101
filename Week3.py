c939c442fb9cc9780d7e1ca8964900ab13e1e57e
bl1zz4rd <shunyuliu@gmail.com>
2017-12-16 10:44:44 +0800

    Create homework for week3

---
+++ Week3.py
@@ -0,0 +1,33 @@
+print "-------Homework 1---------"
+a = 0b1010
+print "a =", (bin(a))
+b = 0o0011
+print "b =", (oct(b))
+c = 64
+print "c =", c
+d = 0x0011
+print "d =", (hex(d))
+print "--------------------------"
+print "a + b + c + d = ", a+b+c+d
+
+print "-------Homework 2---------"
+print 'C:\Documents and Settings\nricsk\test.dat'
+print "--------------------------"
+print 'C:\Documents and Settings\\nricsk\\test.dat'
+
+
+print "-------Homework 3---------"
+print r"\\10.130.200.76\tw_ps\ps_files\Tools"
+print "--------------------------"
+print "\\\\10.130.200.76\\tw_ps\ps_files\Tools"
+
+print "-------Homework 4---------"
+temp =  '''
+Usage: Thingy [OPTIONS]
+     -h                         Display this usage message
+     -H hostname                Hostname to connect to
+        '''
+print temp
+print "--------------------------"
+
+
